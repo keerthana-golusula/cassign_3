fad0f4fd4c5be46473aae1a2d66da50499deb763
keerthana-golusula <Keerthanag9298@gmail.com>
2019-02-05 23:12:05 -0800

    separation of positive and negative integers not completed

---
+++ separatepos_negdigits.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main() {
+	int arr[] = { -12, 11, -13, -5, 6, -7, 5, -3, -6 };
+	int i, k, j;
+	for (i = 0; i < 9; i++) {
+		if (arr[0] < 0){
+			continue;
+		}
+		else if (arr[i] <=0) {
+			k = arr[i];
+			j = i - 1;
+			while (arr[j] > 0) {
+				arr[j+1] = arr[j];
+				j--;
+			}
+			arr[j + 1] = k;
+		}
+
+	   }
+	for (i = 0; i < 9; i++) {
+		printf("%d", arr[i]);
+	}
+
+
+
+}
