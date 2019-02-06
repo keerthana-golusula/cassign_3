8d2ca613a07536a6cc42c3121cd5f35892b47fac
keerthana-golusula <Keerthanag9298@gmail.com>
2019-02-06 02:06:08 -0800

    separation of positive and negative integers program

--- separatepos_negdigits.c
+++ separatepos_negdigits.c
@@ -3,10 +3,10 @@ int main() {
 	int arr[] = { -12, 11, -13, -5, 6, -7, 5, -3, -6 };
 	int i, k, j;
 	for (i = 0; i < 9; i++) {
-		if (arr[0] < 0){
+		if (i==0&&arr[i]<0){
 			continue;
 		}
-		else if (arr[i] <=0) {
+		else if (arr[i] <0) {
 			k = arr[i];
 			j = i - 1;
 			while (arr[j] > 0) {
@@ -18,7 +18,7 @@ int main() {
 
 	   }
 	for (i = 0; i < 9; i++) {
-		printf("%d", arr[i]);
+		printf("%d ", arr[i]);
 	}
 
 
